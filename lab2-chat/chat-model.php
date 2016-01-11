aaef0555c21499fbd81f306dec7b0395399e7e93
Conrad Z <conradzimney@gmail.com>
2016-01-11 13:37:51 -0800

    Almost working...

--- lab2-chat/chat-model.php
+++ lab2-chat/chat-model.php
@@ -6,10 +6,32 @@ class Chat {
         $this->conn = $conn;
     }
     
-    public function search() { 
-        $sql = 'select * from message';
+    public function load() { 
+        $sql = 'select * from message order by sent_timestamp desc limit 10';
         $stmt = $this->conn->prepare($sql);
         $success = $stmt->execute(array());
+        if (!$success) {
+            var_dump($stmt->errorInfo());
+            // trigger_error($stmt->errorInfo());
+            return false;
+        } else {
+            return $stmt->fetchAll();
+        }
+    }
+    
+    public function insert($name, $m) {
+        $name = $_GET['name'];
+        $m = $_GET['m'];
+        if ($name == NULL) {
+            $m = 'This message has no content';
+        }
+        if ($m == NULL) {
+            $name = 'anonymous';   
+        }
+            
+        $sql = 'INSERT INTO message (nickname, content) VALUES (?, ?)';
+        $stmt = $this->conn->prepare($sql);
+        $success = $stmt->execute(array($name,$m));
         if (!$success) {
             var_dump($stmt->errorInfo());
             // trigger_error($stmt->errorInfo());
--- lab2-chat/index.php
+++ lab2-chat/index.php
@@ -3,9 +3,12 @@
 require_once 'connection.php';
 require_once 'chat-model.php';
 
+$name = $_GET['name'];
+$m = $_GET['m'];
+
 $conn = getConnection();
 $chatModel = new Chat($conn);
-$messages = $chatModel->search();
+$messages = $chatModel->load();
 
 ?>
 
@@ -14,7 +17,6 @@ $messages = $chatModel->search();
 <head>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta charset="UTF-8">
-    <link rel="icon" href="img/page-icon.png">
     <title>Chat</title>
     
     <!-- bootstrap css -->
@@ -23,14 +25,48 @@ $messages = $chatModel->search();
 </head>
 <body class="container">
  
-    <h1>Messages</h1>
-        <ul>
-            <?php foreach($messages as $message):?>
-            <li>
-                <?= htmlentities($message['content']) ?>
-            </li>
-            <?php endforeach; ?>
-        </ul>
+    <h1>Lab 2: Info 344 Class Chat</h1>
+    <!-- Message input area -->  
+    <form action="" method="GET">
+        <div class="form-group">
+            <p>Nickname:</p>
+            <input type="text" 
+                id="nameInput" 
+                name="name"
+                class="form-control" 
+                value="<?= htmlentities($name) ?>"
+                placeholder="Nickname (anonymous by default)"
+                required
+                >
+        </div>
+        <p>Message Content:</p>
+        <div class="form-group">
+            <input type="text" 
+                id="mInput" 
+                name="m"
+                class="form-control" 
+                value="<?= htmlentities($m) ?>"
+                placeholder="Message you would like to send"
+                required
+                >
+        </div>
+        <div class="form-group">
+            <button class="btn btn-primary" type="submit" onclick="<?php $chatModel->insert($name, $m)?>">Send Message</button>
+        </div>
+    </form>
+    <br>
+    <br>
+    
+    <!-- Messages -->
+    <ul>
+        <?php foreach($messages as $message):?>
+        <li>
+            <?= htmlentities($message['nickname']) ?>: 
+            <?= htmlentities($message['content']) ?>
+            <?= htmlentities($message['sent_timestamp']) ?>
+        </li>
+        <?php endforeach; ?>
+    </ul>
     
    
 </body>
