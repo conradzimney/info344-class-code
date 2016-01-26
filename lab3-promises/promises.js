fed046ffd6ba6e6ccc80a7d0e9303e734430131c
Conrad Z <conradzimney@gmail.com>
2016-01-26 10:41:20 -0800

    Everything but movie poster

--- lab3-promises/promises.js
+++ lab3-promises/promises.js
@@ -47,6 +47,62 @@ function getMovie(movieId) {
         });   
 };
 
-getMovie('tt0120737');
+// getMovie('tt0120737');
+
+function getThreeMovies(id1, id2, id3) {
+    get("http://www.omdbapi.com/?i=" + id1 + "&plot=short&r=json")
+        .then(function(url) {
+            console.log(JSON.parse(url));
+        })
+        .catch(function(err) {
+            console.error(err);
+        })
+    get("http://www.omdbapi.com/?i=" + id2 + "&plot=short&r=json")
+        .then(function(url) {
+            console.log(JSON.parse(url));    
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
+    get("http://www.omdbapi.com/?i=" + id3 + "&plot=short&r=json")
+        .then(function(url) {
+            console.log(JSON.parse(url));
+        })
+        .catch(function(err) {
+            console.error(err);
+        });            
+};
+
+// getThreeMovies('tt0120737','tt0120737','tt0120737');
+
+function getThreeMoviesConcurrently(id1, id2, id3) {
+    Promise.all([get("http://www.omdbapi.com/?i=" + id1 + "&plot=short&r=json"), get("http://www.omdbapi.com/?i=" + id2 + "&plot=short&r=json"), get("http://www.omdbapi.com/?i=" + id3 + "&plot=short&r=json")])
+        .then(function(urls) {
+            urls.forEach(function(url) {
+                console.log(JSON.parse(url));    
+            });    
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
+};
+
+// getThreeMoviesConcurrently('tt0120737','tt0120737','tt0120737');
+
+function getMoviePoster(movieId) {
+    var movieJSON = {};
+    get("http://www.omdbapi.com/?i=" + movieId + "&plot=short&r=json")
+        .then(function(url) {
+            movieJSON = JSON.parse(url);
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
+    console.log(movieJSON);
+    //var image = movieJSON['Poster'];
+    //console.log(image);
+};
+
+getMoviePoster('tt0120737');
 
 
